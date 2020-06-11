e17a27e5b6f38e950f6664a60bd3771d9f6d8523
dethboy <dethboy@macintosh.home>
2020-06-11 14:25:37 +0300

    Add name paremeter

--- myFirstFunction.js
+++ myFirstFunction.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 exports.handler = async (event) => {
+    const name = event.queryStringParameters && event.queryStringParameters.name;
+    
+    let message = "Hello Lambda";
+    
+    if (name != null) {
+        message = `Hello ${name}!`;
+    }
+    
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello Lambda!')
+        body: JSON.stringify(message)
     };
     
     return response;
